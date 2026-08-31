b026ded1dcffd6f9b41005467fb1f15629e91af6
Tuan Anh <90910152+anhalexle@users.noreply.github.com>
2026-08-31 23:24:10 +0700

    Add: sqrtx - submission-0

---
+++ Data Structures & Algorithms/sqrtx/submission-0.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        int l = 0, r = x;
+        int res = 0;
+        while (l <= r)
+        {
+            int m = l + (r - l) / 2;
+            if (pow(m, 2) > x)
+            {
+                r = m - 1;
+            }
+            else if (pow(m, 2) < x)
+            {
+                l = m + 1;
+                res = m;
+            }
+            else
+                return m;
+        }
+        return res;
+    }
+};
