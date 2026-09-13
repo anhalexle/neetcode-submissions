6563914a48c6c8c0740caea5840ef66d2c231ffe
Tuan Anh <90910152+anhalexle@users.noreply.github.com>
2026-09-13 12:21:30 +0700

    Add: maximum-frequency-stack - submission-1

---
+++ Data Structures & Algorithms/maximum-frequency-stack/submission-1.cpp
@@ -0,0 +1,92 @@
+class FreqStack {
+public:
+    stack<int> myStack;
+    map<int, int> myMap;
+    int max = 0;
+    FreqStack() {
+    
+    }
+    
+    void push(int val) {
+        myMap[val]++;
+        myStack.push(val);
+        updateMax();
+    }
+
+    void updateMax()
+    {
+        int tempMax = 0;
+        for (auto &it : myMap)
+        {
+            if (it.second > tempMax)
+            {
+                tempMax = it.second;
+            }
+        }
+        max = tempMax;
+    }
+    
+    int pop() {
+        int topVal = 0;
+        // if (myMap[topVal] == max)
+        // {
+        //     myStack.pop();
+        //     myMap[topVal]--;
+        // }
+        // else
+        // {
+        //     vector<int> myVector;
+        //     while (!myStack.empty())
+        //     {
+        //         myVector.push_back(topVal);
+        //         myStack.pop();
+        //         topVal = myStack.top();
+        //         if (myMap[topVal] == max)
+        //         {
+        //             myStack.pop();
+        //             myMap[topVal]--;
+        //             break;
+        //         }
+        //     }
+        //     if (!myVector.empty())
+        //     {
+        //         for (int i = myVector.size() - 1; i>= 0; i--)
+        //         {
+        //             myStack.push(myVector[i]);
+        //         }
+        //     }
+        // }
+        vector<int> myVector;
+        while(!myStack.empty())
+        {
+            topVal = myStack.top();
+            if (myMap[topVal] == max)
+            {
+                myStack.pop();
+                myMap[topVal]--;
+                break;
+            }
+            else
+            {
+                myVector.push_back(topVal);
+                myStack.pop();
+            }
+        }
+        if (!myVector.empty())
+        {
+            for (int i = myVector.size() - 1; i>= 0; i--)
+            {
+                myStack.push(myVector[i]);
+            }
+        }
+        updateMax();
+        return topVal;
+    }
+};
+
+/**
+ * Your FreqStack object will be instantiated and called as such:
+ * FreqStack* obj = new FreqStack();
+ * obj->push(val);
+ * int param_2 = obj->pop();
+ */
